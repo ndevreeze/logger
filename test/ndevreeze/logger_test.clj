2453373c8c4562fd0d223272934cc9003cbd2154
ndevreeze <nico@ndv.nl>
2020-12-21 18:48:40 +0100

    Add test for logger

--- test/ndevreeze/logger_test.clj
+++ test/ndevreeze/logger_test.clj
@@ -1,13 +1,17 @@
 (ns ndevreeze.logger-test
     (:require [midje.sweet :as midje]
-              [ndevreeze.logger :refer :all]))
+              [ndevreeze.logger :as log]))
 
-#_(midje/facts
- "Test several facts"
+(midje/facts
+ "Test logger"
 
  ;; 2020-12-19: a bit strange, with dummy as postfix and prefix.
- (midje/fact "Test dummy function"
-             (logger-dummy) => "Dummy-logger")
+ (midje/fact "Test logger function"
+             (do
+               (log/init "log.out" :info)
+               (log/info "Log at info")
+               (slurp "log.out"))
+             => "Log at info")
  )
 
  
