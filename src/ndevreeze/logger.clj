6d06488be55921c2f6e4432e84c2221810e1300c
ndevreeze <nico@ndv.nl>
2021-04-14 21:57:27 +0200

    Init with a map or with old ways
    Both with a map and old way with filename and log-level should work now.

--- src/ndevreeze/logger.clj
+++ src/ndevreeze/logger.clj
@@ -182,8 +182,8 @@
   [{:keys [cwd name] :as opts} pattern]
   (str/replace pattern #"%([hcstnd])" (fn [[_ letter]] (replace-letter opts letter))))
 
-(defn init
-  "Initialises a log file.
+(defn init-with-map
+  "Initialises logging, possibly with a log-file.
 
   level     - log level like :info, :debug
   file      - give an explicit full path. Default is nil, use pattern
@@ -195,7 +195,7 @@
 
   if all of file, pattern and location are nil, do not create a
   logfile, just log to the console.
-  
+
   To use in pattern:
    - %h = home dir
    - %c = current dir
@@ -220,3 +220,76 @@
     (if overwrite
       (fs/delete path))
     (init-internal path level)))
+
+(defn init
+  "Initialises logging, possibly with a log-file.
+
+  If called with:
+  - 0 parameters - init with no logfile at level :info
+  - 1 parameter that is a map - see below.
+  - 1 parameter that is a string or file - logfile, level will be :info
+  - 2 parameters - a logfile and a log level
+
+  level     - log level like :info, :debug
+  file      - give an explicit full path. Default is nil, use pattern
+  pattern   - use a pattern for the log file path, see below
+  location  - some pattern defaults/shortcuts, :home, :cwd, :script, :temp, default is nil
+  name      - script name, \"script\" bij default
+  cwd       - give an explicit current-working-dir, default is nil
+  overwrite - boolean, overwrite an existing log file, default false
+
+  if all of file, pattern and location are nil, do not create a
+  logfile, just log to the console.
+
+  To use in pattern:
+   - %h = home dir
+   - %c = current dir
+   - %s = script dir (TBD)
+   - %t = temp dir (/tmp, or c:/tmp)
+   - %n = script name
+   - %d = datetime, as yyyy-mm-ddTHH-MM-SS"
+  ([par1]
+   (if (map? par1)
+     (init-with-map par1)
+     (init-internal par1 :info)))
+  ([logfile loglevel] (init-internal logfile loglevel))
+  ([] (init-internal nil :info)))
+
+#_(defn init
+    "Initialises a log file.
+
+  level     - log level like :info, :debug
+  file      - give an explicit full path. Default is nil, use pattern
+  pattern   - use a pattern for the log file path, see below
+  location  - some pattern defaults/shortcuts, :home, :cwd, :script, :temp, default is nil
+  name      - script name, \"script\" bij default
+  cwd       - give an explicit current-working-dir, default is nil
+  overwrite - boolean, overwrite an existing log file, default false
+
+  if all of file, pattern and location are nil, do not create a
+  logfile, just log to the console.
+
+  To use in pattern:
+   - %h = home dir
+   - %c = current dir
+   - %s = script dir (TBD)
+   - %t = temp dir (/tmp, or c:/tmp)
+   - %n = script name
+   - %d = datetime, as yyyy-mm-ddTHH-MM-SS"
+    [{:keys [level file pattern location name cwd overwrite] :as opts
+      :or {level :info
+           file nil
+           pattern "%h/log/%n-%d.log"
+           location nil
+           name "script"
+           cwd nil
+           overwrite false}}]
+    (let [pattern (if location
+                    (to-pattern location)
+                    pattern)
+          path (if file
+                 (-> file fs/expand-home fs/absolute str)
+                 (to-log-file opts pattern))]
+      (if overwrite
+        (fs/delete path))
+      (init-internal path level)))
