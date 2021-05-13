6e59c1cb26134ba0b6318ffa9458647e58bad6e6
ndevreeze <nico@ndv.nl>
2021-05-13 14:13:56 +0200

    Bump used version of clojure and midje

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ndevreeze/logger"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [clj-commons/fs "1.6.307"]
                  [log4j "1.2.17"]
                  [clojure.java-time "0.3.2"] ;;; new in Java 8, replacing Joda-time
@@ -14,7 +14,7 @@
 
   :resource-paths ["resources"]
   
-  :profiles {:dev {:dependencies [[midje "1.9.9"]]}}
+  :profiles {:dev {:dependencies [[midje "1.10.3"]]}}
 
   :repl-options {:init-ns ndevreeze.logger}
 
