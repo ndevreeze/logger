31468a434c685fb0bf5617f71f4c279a0a6a9719
ndevreeze <nico@ndv.nl>
2022-01-16 21:05:01 +0100

    Bump used libraries to newest versions
    clj-commons/fs, java-time and midje.

--- project.clj
+++ project.clj
@@ -4,18 +4,16 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [clj-commons/fs "1.6.307"]
+                 [clj-commons/fs "1.6.310"]
                  [org.apache.logging.log4j/log4j-core "2.17.1"]
                  [org.apache.logging.log4j/log4j-api "2.17.1"]
-                 [clojure.java-time "0.3.2"] ;;; new in Java 8, replacing Joda-time
-                                             ;;; and clj-time
-                 ]
+                 [clojure.java-time "0.3.3"]]
 
   :target-path "target/%s"
 
   :resource-paths ["resources"]
   
-  :profiles {:dev {:dependencies [[midje "1.10.3"]]}}
+  :profiles {:dev {:dependencies [[midje "1.10.5"]]}}
 
   :repl-options {:init-ns ndevreeze.logger}
 
