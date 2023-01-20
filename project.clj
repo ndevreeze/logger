23e6719eb7daf6d8d8bbb1053354f39f5dafde0b
ndevreeze <nico@ndv.nl>
2023-01-20 19:27:00 +0100

    Upgrade dependencies
    Thanks to lein ancient.

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
-(defproject ndevreeze/logger "0.6.0"
+(defproject ndevreeze/logger "0.6.1"
   :description "Small log4j2 logging wrapper"
   :url "https://github.com/ndevreeze/logger"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-commons/fs "1.6.310"]
-                 [org.apache.logging.log4j/log4j-core "2.17.1"]
-                 [org.apache.logging.log4j/log4j-api "2.17.1"]
-                 [clojure.java-time "0.3.3"]]
+                 [org.apache.logging.log4j/log4j-core "2.19.0"]
+                 [org.apache.logging.log4j/log4j-api "2.19.0"]
+                 [clojure.java-time "1.2.0"]]
 
   :target-path "target/%s"
 
   :resource-paths ["resources"]
   
-  :profiles {:dev {:dependencies [[midje "1.10.5"]]}}
+  :profiles {:dev {:dependencies [[midje "1.10.9"]]}}
 
   :repl-options {:init-ns ndevreeze.logger}
 
