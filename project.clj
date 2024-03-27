5fd03b2400b79adc809fd5165cc2aa9976058aa5
ndevreeze <nico@ndv.nl>
2024-03-27 21:23:30 +0100

    Bump library version(s) wrt lein ancient

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/ndevreeze/logger"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [clj-commons/fs "1.6.310"]
-                 [org.apache.logging.log4j/log4j-core "2.22.1"]
-                 [org.apache.logging.log4j/log4j-api "2.22.1"]
+  :dependencies [[org.clojure/clojure "1.11.2"]
+                 [clj-commons/fs "1.6.311"]
+                 [org.apache.logging.log4j/log4j-core "2.23.1"]
+                 [org.apache.logging.log4j/log4j-api "2.23.1"]
                  [clojure.java-time "1.4.2"]]
 
   :target-path "target/%s"
